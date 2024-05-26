69087c5624d7aa8d29f26aaa49f849ef1a7a83b6
RAFEL <rmallqui@gmail.com>
2024-05-26 02:52:43 -0300

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function store(Request $request)
+    {
+        $credentials  = $request->only('email', 'password');
+
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $token = auth()->user()->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+}
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'price' => $this->faker->randomFloat(2, 1, 100),
+            'description' => "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRIZ-zcvTn4xC4qFWWY-1qP5JzBQUDdNcQwbaqxDmePLqWERKNyew3s1nqJwuCFkyCax5-1O0oRvH9OCbIFjdg4vh6vxsL4ILE3ttsXUCnQinbX-w_16L-9sg&usqp=CAE",
+            'image' => $this->faker->imageUrl(),
+                ];
+    }
+}
